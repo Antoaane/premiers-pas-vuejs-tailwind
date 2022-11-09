2aad1f5cda8d636e69d1cabe9c64b2cf7f21cadb
Antoaane <antoine.lauzis@etudiant.univ-reims.fr>
2022-11-09 15:02:39 +0100

    modification scroll hidden

---
+++ src/components/Picture.vue
@@ -0,0 +1,13 @@
+<template>
+    <img class="picture" :src="img" v-if="init == true" alt="">
+    <img class="picture" src="../assets/hero-image.jpg" v-else alt="">
+</template>
+
+<script>
+export default{
+    props: {
+        img: String,
+        init: Boolean
+    }
+}
+</script>
